3a1c2111ec5a3af4dd895dfea08eda8a34053976
Pavel Kuznetsov <Technic47@gmail.com>
2025-01-28 23:25:40 +0300

    Non vpn compose-file created.

--- src/main/java/ru/kuznetsov/elfin/config/ElasticSearchConfig.java
+++ src/main/java/ru/kuznetsov/elfin/config/ElasticSearchConfig.java
@@ -4,13 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchClients;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "ru.kuznetsov.elfin.repositories")
-@ComponentScan(basePackages = { "ru.kuznetsov.elfin.models.entity" })
+@ComponentScan(basePackages = {"ru.kuznetsov.elfin.models.entity"})
 @RequiredArgsConstructor
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
