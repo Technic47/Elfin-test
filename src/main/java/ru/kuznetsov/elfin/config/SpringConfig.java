0b11b418cc5b8fd292b1f96642ae98b6349b5a3a
Pavel Kuznetsov <Technic47@gmail.com>
2025-02-12 17:30:24 +0300

    Camunda dependency chenged to camunda-engine-dmn only.

--- src/main/java/ru/kuznetsov/elfin/config/SpringConfig.java
+++ src/main/java/ru/kuznetsov/elfin/config/SpringConfig.java
@@ -1,26 +1,17 @@
 package ru.kuznetsov.elfin.config;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.camunda.bpm.application.PostDeploy;
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.dmn.engine.DmnEngine;
 import org.camunda.bpm.dmn.engine.DmnEngineConfiguration;
-import org.camunda.bpm.engine.DecisionService;
-import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.variable.VariableMap;
-import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-@ProcessApplication("Grade client")
 public class SpringConfig {
 
     @Bean
-    public DmnEngine getDmnEngine(){
+    public DmnEngine getDmnEngine() {
         return DmnEngineConfiguration
                 .createDefaultDmnEngineConfiguration()
                 .buildEngine();
